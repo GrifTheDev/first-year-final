979fca04a11d14cdb1c82e5aad37b9a41ddf5082
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-04-27 21:52:25 +0200

    ✅ First NodeJS done.

---
+++ 1. Prime Number Sieve/NodeJS/main.js
@@ -0,0 +1,12 @@
+const toReach = 100000
+
+for (var i = 2; i < toReach; i++) {
+    var isPrime = true
+    for (var j = 2; j < i; j++) {
+        if(i % j == 0) {
+            isPrime = false
+        }
+    }
+
+    if (isPrime == true) console.log(i)
+}

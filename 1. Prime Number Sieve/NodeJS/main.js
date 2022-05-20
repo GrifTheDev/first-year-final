cec929f9a1e88dcad9f8286cf4997fd3a7f6b704
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-20 09:18:28 +0200

    🐰 Small refactor

--- 1. Prime Number Sieve/NodeJS/main.js
+++ 1. Prime Number Sieve/NodeJS/main.js
@@ -1,12 +1,12 @@
-const toReach = 100000 // 100,000
+const toReach = 100000; // 100,000
 
 for (var i = 2; i < toReach; i++) {
-    var isPrime = true
-    for (var j = 2; j < i; j++) {
-        if(i % j == 0) {
-            isPrime = false
-        }
+  var isPrime = true;
+  for (var j = 2; j < i; j++) {
+    if (i % j == 0) {
+      isPrime = false;
     }
+  }
 
-    if (isPrime == true) console.log(i)
+  if (isPrime == true) console.log(i);
 }

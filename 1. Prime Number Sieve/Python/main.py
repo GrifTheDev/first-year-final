7a6218dcb1e3df19753b5b2ecaa4dbf40b30dbea
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-20 08:26:18 +0200

    🛠 Refactor code to match original NodeJS code

--- 1. Prime Number Sieve/Python/main.py
+++ 1. Prime Number Sieve/Python/main.py
@@ -1,7 +1,11 @@
-for i in range(2, 100000+1):
-     zbroj=0
-     for j in range(2, i):
-          if i%j==0:
-               zbroj+=1
-     if zbroj==0:
-          print(i)
+from pickle import TRUE
+
+
+toReach = 100000  # 100,000
+for i in range(2, toReach):
+    isPrime = True
+    for j in range(2, i):
+        if i % j == 0:
+            isPrime = False
+    if isPrime == True:
+        print(i)

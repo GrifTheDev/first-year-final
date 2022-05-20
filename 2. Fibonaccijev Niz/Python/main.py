ee881998085c36729815cb6c3570dfa0a8a1bf22
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-20 08:58:06 +0200

    ❌Makni Fibonacijev Niz

--- 2. Fibonaccijev Niz/Python/main.py
+++
@@ -1,7 +0,0 @@
-fibonaccivar1=1
-fibonaccivar2=0
-while(fibonaccivar1<1000000000):
-     print(fibonaccivar1, sep="\n")
-     fibonaccivar3=fibonaccivar1
-     fibonaccivar1+=fibonaccivar2
-     fibonaccivar2=fibonaccivar3

cb81d4554b5c39066a48452564e00d8b3ae44dcb
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-20 12:45:49 +0200

    😎 Python 3x+1

---
+++ 2. Problem 3x + 1/Python/main.py
@@ -0,0 +1,13 @@
+targetNumber = 5000
+for i in range(10, targetNumber + 1):
+    print("==========", i, "==========")
+    j = i
+    while (j > 2):
+        if (j % 2 == 0):
+            j //= 2
+            print(j)
+        else:
+            j = 3 * j + 1
+            print(j)
+
+print(j / 2)

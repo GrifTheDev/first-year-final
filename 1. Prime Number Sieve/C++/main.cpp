ba68550fd1a8b4269b3d6d82c7b84d52275ce598
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-20 09:04:41 +0200

    🛠 Refactor Code

--- 1. Prime Number Sieve/C++/main.cpp
+++ 1. Prime Number Sieve/C++/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-int n = 2, toc; 
-int main() {
-  while(n < 100000){
-    toc = 0;
-    for(int i = 2; i < sqrt(n)+1; i++){
-      if(n % i == 0){
-        toc = 1;
+
+int main()
+{
+  int toReach = 100000; // 100,000
+
+  for (int i = 2; i < toReach; i++)
+  {
+    bool isPrime = true;
+
+    for (int j = 2; j < i; j++) {
+      if (i % j == 0) {
+        isPrime = false;
       }
     }
-    if(toc == 0){
-      cout << n << endl;
+    if (isPrime == true) {
+      cout << i << endl;
     }
-    n++;
   }
   return 0;
 }

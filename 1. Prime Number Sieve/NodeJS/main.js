67ad6dd88effa4c1aa75e95939e779b5409e0e99
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-20 12:13:28 +0200

    🦙 Push changes to Sieve / Start work on 3x + 1

--- 1. Prime Number Sieve/NodeJS/main.js
+++ 1. Prime Number Sieve/NodeJS/main.js
@@ -1,12 +1,12 @@
 const toReach = 100000; // 100,000
 
-for (var i = 2; i < toReach; i++) {
+for (var j = 2; j < toReach; j++) {
   var isPrime = true;
-  for (var j = 2; j < i; j++) {
-    if (i % j == 0) {
+  for (var j = 2; j < j; j++) {
+    if (j % j == 0) {
       isPrime = false;
     }
   }
 
-  if (isPrime == true) console.log(i);
+  if (isPrime == true) console.log(j);
 }
---
+++ 2. Problem 3x + 1/NodeJS/main.js
@@ -0,0 +1,16 @@
+var targetNumber = 5000;
+for (var i = 10; i < targetNumber + 1; i++) {
+  console.log("────────── " + i + " ──────────");
+  var j = i;
+  while (j > 2) {
+    if (j % 2 == 0) {
+      j /= 2;
+      console.log(j);
+    } else {
+      j = 3 * j + 1;
+      console.log(j);
+    }
+  }
+
+  console.log(j / 2);
+}

80ebcadcdb42c1ac01d8878315fc30cb3e0a8d1f
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2022-05-31 16:06:18 +0200

    🛠 Small fix

--- 1. Prime Number Sieve/NodeJS/main.js
+++ 1. Prime Number Sieve/NodeJS/main.js
@@ -1,9 +1,9 @@
 const toReach = 100000; // 100,000
 
-for (var j = 2; j < toReach; j++) {
+for (var i = 2; i < toReach; i++) {
   var isPrime = true;
-  for (var j = 2; j < j; j++) {
-    if (j % j == 0) {
+  for (var j = 2; j < i; j++) {
+    if (i % j == 0) {
       isPrime = false;
     }
   }
